e0559c0e2154d8e6a3c486b95a51918d4dd9173a
Misbah Khan <misbahkhan@Misbahs-MacBook-Pro.local>
2014-07-02 14:45:55 -0700

    4.0 beginning

--- webview/freeAppLifeAppDelegate.m
+++ webview/freeAppLifeAppDelegate.m
@@ -9,13 +9,22 @@
 #import "freeAppLifeAppDelegate.h"
 #import "API.h"
 #import "Flurry.h"
+#import <Parse/Parse.h>
 
 @implementation freeAppLifeAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
-	application.applicationIconBadgeNumber = 0;
-    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:
-     (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+    
+    [Parse setApplicationId:@"VZjp1eLHVpNpvqN5QOTZo24HoWn3BnIzKBTORGiM"
+                  clientKey:@"4xGF0AROHkzm9DYSydmj1cSapsRaFY3mygIEgzu7"];
+    [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|
+     UIRemoteNotificationTypeAlert|
+     UIRemoteNotificationTypeSound];
+    
+//	application.applicationIconBadgeNumber = 0;
+//    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:
+//     (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+    
     if(launchOptions!=nil){
         NSDictionary *msg = [launchOptions objectForKey:@"UIApplicationLaunchOptionsRemoteNotificationKey"];
         [self handleMsg:msg];
@@ -27,34 +36,17 @@
     return YES;
 }
 
-- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken{
-    API *sharedInstance = [API sharedInstance];
-    Log(@"device token: %@", deviceToken);
-    sharedInstance.deviceToken = deviceToken;
-}
-
-- (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error{
-	Log(@"Failed to register with error : %@", error);
-    API *sharedInstance = [API sharedInstance];
-    NSString *postString2 = [NSString stringWithFormat:@"userID=%@&msg=%@", [sharedInstance md5ForString:[sharedInstance serialNumber]], [NSString stringWithFormat:@"%@", error]];
-    NSMutableURLRequest *request3 = [sharedInstance requestForEndpoint:@"apns_error" andBody:postString2];
-    [NSURLConnection sendAsynchronousRequest:request3 queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
-        
-    }];
-//    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"ERROR" message:[NSString stringWithFormat:@"%@", error] delegate:Nil cancelButtonTitle:@"Cancel" otherButtonTitles:nil, nil];
-//    [alert show];
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    // Store the deviceToken in the current installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    [currentInstallation saveInBackground];
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    application.applicationIconBadgeNumber = 0;
-    [self handleMsg:userInfo];
-//    NSError *e;
-//    NSDictionary *JSON =
-//    [NSJSONSerialization JSONObjectWithData: [msg dataUsingEncoding:NSUTF8StringEncoding]
-//                                    options: NSJSONReadingMutableContainers
-//                                      error: &e];
-//    msg = [JSON objectForKey:@"data"];
-//    [self createAlert:msg];
+//    [self handleMsg:userInfo];
+    [PFPush handlePush:userInfo];
 }
 
 - (void) handleMsg:(NSDictionary *)userInfo{
